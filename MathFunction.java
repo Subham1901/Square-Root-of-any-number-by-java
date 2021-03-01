d725c02f3762ebdd48172d1a65645220fea90f89
SUBHAM PRASAD PANDA <79370554+Subham1901@users.noreply.github.com>
2021-03-01 20:43:34 +0530

    Add files via upload

---
+++ MathFunction.java
@@ -0,0 +1,12 @@
+@author Subham
+*/ here i have mentioned that how to print square root of any number b using Math class*/
+import java.util.Scanner;
+public class MathFunction{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter any number: ");
+		int n=sc.nextInt();
+		double sqrt=Math.sqrt(n);
+		System.out.println("square root of " +n+ " is " +sqrt);
+	}
+}
